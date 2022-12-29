a70c5a5dd6454f3b7692f96b06f1b66057b37a09
“kondrashev” <p.kondrashev@alterego.digital>
2022-12-29 20:33:04 +0200

    fix credential for database

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 // @ts-nocheck
 const { Sequelize } = require("sequelize");
 //localserver
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-  }
-);
+// module.exports = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     port: process.env.DB_PORT,
+//   }
+// );
 
 // heroku
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
