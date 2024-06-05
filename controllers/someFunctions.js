624bd673e693eb71bd7cb0e26e8dbb5e48fa8aaa
“kondrashev” <p.kondrashev@alterego.digital>
2024-06-05 15:23:06 +0300

    fixed function "informationUpload" in module "someFunctions"

--- controllers/someFunctions.js
+++ controllers/someFunctions.js
@@ -45,9 +45,9 @@ const informationUpload = (data, group, getDiscipline) => {
     return [surName, groupName, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16];
   });
   const { name } = getDiscipline;
-  if (name === 'Матеріалознавство' || 'СКЖЦВ') {
-    keys = newKeys.map(([, , ...item]) => item.filter((item) => item !== undefined).slice(0, 1));
-    values = newValues.map(([, , ...item]) => item.filter((item) => item !== undefined).slice(0, 1));
+  if ((name === 'Матеріалознавство') | 'СКЖЦВ') {
+    keys = newKeys.map(([, , ...item]) => item.filter((item) => item !== undefined).slice(0, 9));
+    values = newValues.map(([, , ...item]) => item.filter((item) => item !== undefined).slice(0, 9));
   } else if (name === 'ТКМ') {
     keys = newKeys.map(([, , ...item]) => item.filter((item) => item !== undefined));
     values = newValues.map(([, , ...item]) => item.filter((item) => item !== undefined));
